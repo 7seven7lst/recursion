183095feccf8ff607a4b2c75ee6337be7f71b401
Seven Li <sevenlist0110@gmail.com>
2015-02-22 20:43:58 -0800

    Implement stringifyJSON to handle stringifiable objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj ===null){
+    return "null";
+  }
+  else if (obj ===undefined) {
+    return "";
+  }
+  else {
+    var  objType= typeof obj;
+    switch (objType) {
+      case "number":
+      case "boolean":
+      case "symbol":
+      return obj.toString();
+      break;
+      case "string":
+      return ('"'+obj+'"');
+      break;
+      case "object":
+        if (Array.isArray(obj)){ // if it's array
+          var str="[";
+          for (var i=0; i<obj.length; i++) {
+            str+=stringifyJSON(obj[i]);
+            if (i!= obj.length-1){
+              str+=",";
+            }
+          }
+          str+="]";
+          return str;
+        }
+        else { // if it's object
+          var flag=false;
+          var str="{";
+          for (var i in obj){
+            str+=stringifyJSON(i);
+            str+=":";
+            str+=stringifyJSON(obj[i]);
+            str+=",";
+            flag=true;
+          }
+          if (flag===true){
+            str=str.substring(0,str.length-1);
+          }
+          str+="}";
+          return str;
+        }
+      break;
+    }
+  } 
 };
